f0a5b448d86b7427e681129b41ce64d2e8ed697e
Javier Suazo <javier_suazo@live.com>
2015-05-26 23:11:40 +0200

    Initial setup

---
+++ GeoCodingSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geocoding;
+using Geocoding.Google;
+
+namespace GeoCodingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGeocoder geocoder = new GoogleGeocoder(){ApiKey = "your_api_key" };
+            IEnumerable<Address> addresses = geocoder.Geocode("tegucigalpa honduras");
+            Console.WriteLine("Formatted: " + addresses.First().FormattedAddress); 
+            Console.WriteLine("Coordinates: " + addresses.First().Coordinates.Latitude + ", " + addresses.First().Coordinates.Longitude); 
+        }
+    }
+}
